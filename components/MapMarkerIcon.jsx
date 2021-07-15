b555561d58c0529f5bc7a589bd31922d329fbf00
Gonçalo Morais <gnclmorais@gmail.com>
2021-07-15 09:14:36 +0100

    Introduce CSS-only markers
    These are quite flexible and will allow me to customise the map
    further.

--- components/MapMarkerIcon.jsx
+++ components/MapMarkerIcon.jsx
@@ -1,12 +1,20 @@
 import L from 'leaflet';
 
+import styles from './MapMarkerIcon.module.css';
+
+console.log({ styles })
+
 const customIcon = ({ number }) => new L.divIcon({
   className: '',
   iconAnchor: [12, 25],
   labelAnchor: [-6, 0],
   popupAnchor: [0, -15],
   iconSize: [25, 41],
-  html: `<span class="fa fa-industry">${number}</span>`
+  // html: `<span class="fa fa-industry">${number}</span>`,
+  // https://css-tricks.com/adding-shadows-to-svg-icons-with-css-and-svg-filters/
+  html: `<div class="${styles.marker}">
+    <span class="${styles.marker__text}">${number}</span>
+  </div>`,
 });
 
 export { customIcon };
