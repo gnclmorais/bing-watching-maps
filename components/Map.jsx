baed3d9edbd37c451082b9eba7b21b2b75f19d07
Gonçalo Morais <gnclmorais@gmail.com>
2021-10-26 09:35:50 +0100

    Add CLick & Scroll on markers

--- components/Map.jsx
+++ components/Map.jsx
@@ -1,4 +1,6 @@
+import { useEffect, useRef, useState } from 'react';
 import { MapContainer, TileLayer, Marker, Popup, ZoomControl } from 'react-leaflet'
+
 import 'leaflet/dist/leaflet.css'
 import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css'
 import 'leaflet-defaulticon-compatibility';
@@ -9,6 +11,13 @@ const mapboxToken = process.env.NEXT_PUBLIC_MAPBOX_TOKEN;
 const tileLayerUrl = `https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/256/{z}/{x}/{y}@2x?access_token=${mapboxToken}`;
 
 const Map = ({ markers = [] }) => {
+  const [focusedMarkerRef, setFocusedMarkerRef] = useState(null);
+  useEffect(() => {
+    if (!focusedMarkerRef) return;
+
+    focusedMarkerRef.current.scrollIntoView({ behavior: 'smooth' });
+  }, [focusedMarkerRef]);
+
   return (
     <MapContainer
       center={[40.8054, -74.0241]}
@@ -22,13 +31,16 @@ const Map = ({ markers = [] }) => {
         attribution='Map data &copy; <a href=&quot;https://www.openstreetmap.org/&quot;>OpenStreetMap</a> contributors, <a href=&quot;https://creativecommons.org/licenses/by-sa/2.0/&quot;>CC-BY-SA</a>, Imagery &copy; <a href=&quot;https://www.mapbox.com/&quot;>Mapbox</a>'
       />
       
-      {markers.map(({ id, position, description }, index) => (
+      {markers.map(({ id, position, description, ref }, index) => (
         <Marker
           key={id}
           icon={customIcon({ number: index + 1 })}
           position={position}
           draggable={false}
           animate={true}
+          eventHandlers={{
+            click: () => setFocusedMarkerRef(ref),
+          }}
         >
           <Popup>{description}</Popup>
         </Marker>
--- pages/maps/[id].js
+++ pages/maps/[id].js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import dynamic from 'next/dynamic';
+import { useRef } from 'react';
 
 import { getAllMapIds, getMapData } from '../../lib/maps'
 
@@ -11,22 +12,37 @@ export default function Map({ mapData }) {
     id,
     position: [place.lat, place.lng],
     description: name,
+    ref: useRef(),
   }));
 
   return (
-    <>
+    // TODO: Change inline styles with Tailwind classes
+    // TODO: Find the class that makes children not overgrow the parent
+    <div className="flex flex-col" style={{ height: '100%' }}>
       <Head>
         <title>{mapData.title}</title>
       </Head>
 
-      <p>
+      <p className="flex-grow-0 flex-shrink">
         Map: <strong>{mapData.title}</strong> (<em>{mapData.subtitle}</em>)
         <br />
-        {mapData.markers.length} places
+        {formattedMarkers.length} places
       </p>
 
-      <MapWithNoSSR markers={formattedMarkers} />
-    </>
+      {/* TODO: Change inline styles with Tailwind classes */}
+      <div className="flex-grow-0 flex-shrink flex flex-row" style={{ overflowY: 'clip' }}>
+        <div className="overflow-y-scroll">
+          <ol>
+            {formattedMarkers.map((marker, index) => (
+              <li key={marker.id} ref={marker.ref}>{index + 1}. {marker.description}</li>
+            ))}
+          </ol>
+        </div>
+        <div className="flex-grow">
+          <MapWithNoSSR markers={formattedMarkers} />
+        </div>
+      </div>
+    </div>
   )
 }
 
